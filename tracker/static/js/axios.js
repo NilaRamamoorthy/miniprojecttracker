24a977cb92ce97f5cc8c5b4c68e26695b24e1efb
NilaRamamoorthy <nilasibiraj@gmail.com>
2025-09-15 14:15:17 +0530

    completed

--- tracker/static/js/axios.js
+++ tracker/static/js/axios.js
@@ -3,8 +3,24 @@ import axios from 'https://cdn.skypack.dev/axios';
 // Get CSRF token from meta tag
 const csrfToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
 
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let cookie of cookies) {
+      cookie = cookie.trim();
+      if (cookie.startsWith(name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',  // Full backend URL here
+//   baseURL: 'http://127.0.0.1:8000/api/',  
+ baseURL: 'https://miniprojecttracker.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
